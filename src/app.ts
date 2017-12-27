5f7b9242539bae133a367d0386383faba0a69cdb
Vitus Lehner <vituslehner@VitsMac.local>
2017-12-27 17:42:40 +0100

    Initial project setup with TypeScript and TSLint

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+//
+// Copyright (c) 2017 Vitus Lehner. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in
+// the project root for full license information.
+//
+
+import { constants } from "./constants";
+
---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2017 Vitus Lehner. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in
+// the project root for full license information.
+//
+
+export const constants = {
+    constants: {
+        baseUrl: "https://mystudy.leuphana.de"
+    }
+};
